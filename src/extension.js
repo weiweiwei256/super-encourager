5d7661de4a49b69edf9b901b14eb829d8183b88a
weiyajun2688 <weiyajun2688@ipalfish.com>
2019-08-19 10:58:55 +0800

    1. 技术测试 webview与插件双向传递消息

--- src/extension.js
+++ src/extension.js
@@ -8,206 +8,234 @@ const { loadImage, delImages, checkLocalImage } = require('./images.js')
 let timeMeter = null // 计时器
 const ALL_KEYWORD = '**全部**'
 function showEncourager(context, imageNames) {
-  log('展示鼓励页...')
-  if (imageNames.length === 0) {
-    return
-  }
-  let i = Math.floor(Math.random(20180804) * imageNames.length)
-  let name = imageNames[i]
-  let folderName = getSettings('keyword')
-  if (getSettings('isGif')) {
-    folderName += GIF_SUFFIX
-  }
-  let imagePath = `./images/${folderName}/${name}`
-  let panel = vscode.window.createWebviewPanel(
-    name, // viewType
-    'come on!!!', // 视图标题
-    vscode.ViewColumn.Two, // 显示在编辑器的哪个部位
-    {
-      enableScripts: true, // 启用JS，默认禁用
-      retainContextWhenHidden: false, // webview被隐藏时保持状态，避免被重置
-    },
-  )
-  const resourcePath = path.join(context.extensionPath, '/index.html')
-  const dirPath = path.dirname(resourcePath)
-  let html = fs.readFileSync(resourcePath, 'utf-8')
-  html = html.replace('$image_path$', imagePath)
-  // vscode不支持直接加载本地资源，需要替换成其专有路径格式，这里只是简单的将样式和JS的路径替换
-  html = html.replace(/(<link.+?href="|<script.+?src="|<img.+?src=")(.+?)"/g, (m, $1, $2) => {
-    return (
-      $1 +
-      vscode.Uri.file(path.resolve(dirPath, $2))
-        .with({ scheme: 'vscode-resource' })
-        .toString() +
-      '"'
+    log('展示鼓励页...')
+    if (imageNames.length === 0) {
+        return
+    }
+    let i = Math.floor(Math.random(20180804) * imageNames.length)
+    let name = imageNames[i]
+    let folderName = getSettings('keyword')
+    if (getSettings('isGif')) {
+        folderName += GIF_SUFFIX
+    }
+    let imagePath = `./images/${folderName}/${name}`
+    const resourcePath = path.join(context.extensionPath, '/index.html')
+    const dirPath = path.dirname(resourcePath)
+    let html = fs.readFileSync(resourcePath, 'utf-8')
+    html = html.replace('$image_path$', imagePath)
+    // vscode不支持直接加载本地资源，需要替换成其专有路径格式，这里只是简单的将样式和JS的路径替换
+    html = html.replace(/(<link.+?href="|<script.+?src="|<img.+?src=")(.+?)"/g, (m, $1, $2) => {
+        return (
+            $1 +
+            vscode.Uri.file(path.resolve(dirPath, $2))
+                .with({ scheme: 'vscode-resource' })
+                .toString() +
+            '"'
+        )
+    })
+    const resourcePath = path.join(context.extensionPath, '/index.html')
+    const dirPath = path.dirname(resourcePath)
+    let html = fs.readFileSync(resourcePath, 'utf-8')
+    html = html.replace('$image_path$', imagePath)
+    // vscode不支持直接加载本地资源，需要替换成其专有路径格式，这里只是简单的将样式和JS的路径替换
+    html = html.replace(/(<link.+?href="|<script.+?src="|<img.+?src=")(.+?)"/g, (m, $1, $2) => {
+        return (
+            $1 +
+            vscode.Uri.file(path.resolve(dirPath, $2))
+                .with({ scheme: 'vscode-resource' })
+                .toString() +
+            '"'
+        )
+    })
+    panel.webview.html = html
+    panel.webview.html = html
+    panel.webview.postMessage({ command: 'collect' })
+    panel.webview.onDidReceiveMessage(
+        message => {
+            switch (message.command) {
+                case 'collect':
+                    g
+                    console.log('extension receive collected', message)
+                    if (message.value) {
+                        cloneImage(`${folderName}/${name}`, '⭐我的最爱')
+                    } else {
+                        delImages(`⭐我的最爱/${name}`)
+                    }
+                    break
+                default:
+            }
+        },
+        undefined,
+        context.subscriptions,
     )
-  })
-  panel.webview.html = html
-  let timeLast = parseInt(getSettings('timeLast'))
-  if (timeLast !== 0) {
-    // 值为0 则不自动关闭
-    setTimeout(() => {
-      panel.dispose()
-    }, timeLast * 1000)
-  }
+
+    let timeLast = parseInt(getSettings('timeLast'))
+    if (timeLast !== 0) {
+        // 值为0 则不自动关闭
+        setTimeout(() => {
+            panel.dispose()
+        }, timeLast * 1000)
+    }
 }
 
 function initTimer(context) {
-  if (!timeMeter) {
-    let timeSetting
-    if (getSettings('type') === 'time-interval') {
-      timeSetting = '00 */' + getSettings('timeInterval') + ' * * * *'
-    } else if (getSettings('type') === 'natural-hour') {
-      timeSetting = '00 00 * * * *'
-    } else if (getSettings('type') === 'natural-half-hour') {
-      timeSetting = '00 00,30 * * * *'
+    if (!timeMeter) {
+        let timeSetting
+        if (getSettings('type') === 'time-interval') {
+            timeSetting = '00 */' + getSettings('timeInterval') + ' * * * *'
+        } else if (getSettings('type') === 'natural-hour') {
+            timeSetting = '00 00 * * * *'
+        } else if (getSettings('type') === 'natural-half-hour') {
+            timeSetting = '00 00,30 * * * *'
+        }
+        timeMeter = new CronJob(
+            timeSetting,
+            function() {
+                main(context)
+            },
+            null,
+            true,
+        )
     }
-    timeMeter = new CronJob(
-      timeSetting,
-      function() {
-        main(context)
-      },
-      null,
-      true,
-    )
-  }
 }
 function main(context) {
-  let localImages = checkLocalImage()
-  if (localImages.length === 0) {
-    loadImage()
-      .then(newImages => {
-        if (newImages.length === 0) {
-          vscode.window.showErrorMessage('无法获取相关图片，请更改关键字')
-        }
-        showEncourager(context, newImages)
-      })
-      .then(undefined, err => {
-        console.error('err', err)
-      })
-  } else {
-    loadImage(localImages)
-    showEncourager(context, localImages)
-  }
-}
-function activate(context) {
-  log('super encourager is starting!')
-  initTimer(context)
-  let call = vscode.commands.registerCommand('superencourager.call', () => {
-    try {
-      main(context)
-      initTimer(context)
-      vscode.workspace.onDidChangeConfiguration(function(event) {
-        Promise.resolve(event.affectsConfiguration('superencourager.type'))
-          .then(data => {
-            if (data) {
-              //true 代表这个属性已经被修改
-              timeMeter = undefined
-              initTimer(context)
-            }
-          })
-          .then(undefined, err => {
-            console.error('err', err)
-          })
-      })
-      vscode.workspace.onDidChangeConfiguration(function(event) {
-        Promise.resolve(event.affectsConfiguration('superencourager.timeInterval'))
-          .then(data => {
-            if (data) {
-              //true 代表这个属性已经被修改
-              timeMeter = undefined
-              initTimer(context)
-            }
-          })
-          .then(undefined, err => {
-            console.error('err', err)
-          })
-      })
-    } catch (e) {
-      console.error(e)
-    }
-  })
-  let setKeyword = vscode.commands.registerCommand('superencourager.setKeyword', () => {
-    vscode.window
-      .showInputBox()
-      .then(
-        data => {
-          if (data === undefined) {
-            return
-          }
-          setSettings('keyword', data)
-            .then(() => {
-              vscode.window.showInformationMessage(`设置关键词 ${data} 成功!`)
+    let localImages = checkLocalImage()
+    if (localImages.length === 0) {
+        loadImage()
+            .then(newImages => {
+                if (newImages.length === 0) {
+                    vscode.window.showErrorMessage('无法获取相关图片，请更改关键字')
+                }
+                showEncourager(context, newImages)
             })
             .then(undefined, err => {
-              console.error('err', err)
+                console.error('err', err)
             })
-        },
-        () => {},
-      )
-      .then(undefined, err => {
-        console.error('err', err)
-      })
-  })
-  let switchKeyword = vscode.commands.registerCommand('superencourager.switchKeyword', () => {
-    let defaultSelect = ['邓紫棋', '石原里美', '火影忍者', '刺客信条', '极品飞车']
-    let select = getKeywords().concat(defaultSelect)
-    vscode.window
-      .showQuickPick(select)
-      .then(data => {
-        if (data === undefined) {
-          return
+    } else {
+        loadImage(localImages)
+        showEncourager(context, localImages)
+    }
+}
+function activate(context) {
+    log('super encourager is starting!')
+    initTimer(context)
+    let call = vscode.commands.registerCommand('superencourager.call', () => {
+        try {
+            main(context)
+            initTimer(context)
+            vscode.workspace.onDidChangeConfiguration(function(event) {
+                Promise.resolve(event.affectsConfiguration('superencourager.type'))
+                    .then(data => {
+                        if (data) {
+                            //true 代表这个属性已经被修改
+                            timeMeter = undefined
+                            initTimer(context)
+                        }
+                    })
+                    .then(undefined, err => {
+                        console.error('err', err)
+                    })
+            })
+            vscode.workspace.onDidChangeConfiguration(function(event) {
+                Promise.resolve(event.affectsConfiguration('superencourager.timeInterval'))
+                    .then(data => {
+                        if (data) {
+                            //true 代表这个属性已经被修改
+                            timeMeter = undefined
+                            initTimer(context)
+                        }
+                    })
+                    .then(undefined, err => {
+                        console.error('err', err)
+                    })
+            })
+        } catch (e) {
+            console.error(e)
         }
-        setSettings('keyword', data)
-          .then(() => {
-            vscode.window.showInformationMessage(`切换关键词 ${data} 成功！`)
-          })
-          .then(undefined, err => {
-            console.error('err', err)
-          })
-      })
-      .then(undefined, err => {
-        console.error('err', err)
-      })
-  })
-  let clearImage = vscode.commands.registerCommand('superencourager.clearImage', () => {
-    let keywordFolder = fs.readdirSync(getImageRootPath())
-    let keywords = getKeywords()
-    keywords.push(ALL_KEYWORD)
-    vscode.window
-      .showQuickPick(keywords)
-      .then(
-        data => {
-          log(data)
-          if (data === undefined) {
-            return
-          }
-          if (data !== ALL_KEYWORD) {
-            delImages(path.join(context.extensionPath, '/images/' + data))
-            // 同时移出动图文件夹
-            delImages(path.join(context.extensionPath, '/images/' + data + GIF_SUFFIX))
-          } else {
-            keywordFolder.forEach(item => {
-              delImages(path.join(context.extensionPath, '/images/' + item))
-              // 同时移出动图文件夹
-              delImages(path.join(context.extensionPath, '/images/' + item + GIF_SUFFIX))
+    })
+    let setKeyword = vscode.commands.registerCommand('superencourager.setKeyword', () => {
+        vscode.window
+            .showInputBox()
+            .then(
+                data => {
+                    if (data === undefined) {
+                        return
+                    }
+                    setSettings('keyword', data)
+                        .then(() => {
+                            vscode.window.showInformationMessage(`设置关键词 ${data} 成功!`)
+                        })
+                        .then(undefined, err => {
+                            console.error('err', err)
+                        })
+                },
+                () => {},
+            )
+            .then(undefined, err => {
+                console.error('err', err)
             })
-          }
-        },
-        () => {},
-      )
-      .then(undefined, err => {
-        console.error('err', err)
-      })
-  })
-  let showPath = vscode.commands.registerCommand('superencourager.showPath', () => {
-    vscode.window.showInformationMessage('超级鼓励师本地资源路径：' + getImageRootPath())
-  })
-  context.subscriptions.push(call)
-  context.subscriptions.push(setKeyword)
-  context.subscriptions.push(switchKeyword)
-  context.subscriptions.push(clearImage)
-  context.subscriptions.push(showPath)
+    })
+    let switchKeyword = vscode.commands.registerCommand('superencourager.switchKeyword', () => {
+        let defaultSelect = ['⭐我的最爱']
+        let select = getKeywords().concat(defaultSelect)
+        vscode.window
+            .showQuickPick(select)
+            .then(data => {
+                if (data === undefined) {
+                    return
+                }
+                setSettings('keyword', data)
+                    .then(() => {
+                        vscode.window.showInformationMessage(`切换关键词 ${data} 成功！`)
+                    })
+                    .then(undefined, err => {
+                        console.error('err', err)
+                    })
+            })
+            .then(undefined, err => {
+                console.error('err', err)
+            })
+    })
+    let clearImage = vscode.commands.registerCommand('superencourager.clearImage', () => {
+        let keywordFolder = fs.readdirSync(getImageRootPath())
+        let keywords = getKeywords()
+        keywords.push(ALL_KEYWORD)
+        vscode.window
+            .showQuickPick(keywords)
+            .then(
+                data => {
+                    log(data)
+                    if (data === undefined) {
+                        return
+                    }
+                    if (data !== ALL_KEYWORD) {
+                        delImages(path.join(context.extensionPath, '/images/' + data))
+                        // 同时移出动图文件夹
+                        delImages(path.join(context.extensionPath, '/images/' + data + GIF_SUFFIX))
+                    } else {
+                        keywordFolder.forEach(item => {
+                            delImages(path.join(context.extensionPath, '/images/' + item))
+                            // 同时移出动图文件夹
+                            delImages(
+                                path.join(context.extensionPath, '/images/' + item + GIF_SUFFIX),
+                            )
+                        })
+                    }
+                },
+                () => {},
+            )
+            .then(undefined, err => {
+                console.error('err', err)
+            })
+    })
+    let showPath = vscode.commands.registerCommand('superencourager.showPath', () => {
+        vscode.window.showInformationMessage('超级鼓励师本地资源路径：' + getImageRootPath())
+    })
+    context.subscriptions.push(call)
+    context.subscriptions.push(setKeyword)
+    context.subscriptions.push(switchKeyword)
+    context.subscriptions.push(clearImage)
+    context.subscriptions.push(showPath)
 }
 
 exports.activate = activate
@@ -215,6 +243,6 @@ exports.activate = activate
 function deactivate() {}
 
 module.exports = {
-  activate,
-  deactivate,
+    activate,
+    deactivate,
 }
--- src/util.js
+++ src/util.js
@@ -5,86 +5,90 @@ const { getSettings } = require('./settings.js')
 let out = null // 终端输出对象
 const GIF_SUFFIX = '_GIF'
 function getImageRootPath() {
-  return path.join(
-    vscode.extensions.getExtension('runnerup.super-encourager').extensionPath,
-    '/images/',
-  )
+    return path.join(
+        vscode.extensions.getExtension('runnerup.super-encourager').extensionPath,
+        '/images/',
+    )
 }
 exports.GIF_SUFFIX = GIF_SUFFIX
 exports.uncompile = function(r) {
-  const n = /(_z2C\$q|_z&e3B|AzdH3F)/g
-  const t = /([a-w\d])/g
-  const e = {
-    w: 'a',
-    k: 'b',
-    v: 'c',
-    1: 'd',
-    j: 'e',
-    u: 'f',
-    2: 'g',
-    i: 'h',
-    t: 'i',
-    3: 'j',
-    h: 'k',
-    s: 'l',
-    4: 'm',
-    g: 'n',
-    5: 'o',
-    r: 'p',
-    q: 'q',
-    6: 'r',
-    f: 's',
-    p: 't',
-    7: 'u',
-    e: 'v',
-    o: 'w',
-    8: '1',
-    d: '2',
-    n: '3',
-    9: '4',
-    c: '5',
-    m: '6',
-    0: '7',
-    b: '8',
-    l: '9',
-    a: '0',
-    _z2C$q: ':',
-    '_z&e3B': '.',
-    AzdH3F: '/',
-  }
-  let o = r.replace(n, function(t, n) {
-    return e[n]
-  })
-  return o.replace(t, function(t, n) {
-    return e[n]
-  })
-}
-exports.getImageRootPath = getImageRootPath
-exports.getImagePath = function() {
-  if (!getSettings('isGif')) {
-    return getImageRootPath() + getSettings('keyword')
-  } else {
-    return getImageRootPath() + getSettings('keyword') + GIF_SUFFIX
-  }
-}
-exports.log = function(msg) {
-  if (!out) {
-    out = vscode.window.createOutputChannel('super encourager')
-    out.show()
-  }
-  out.appendLine(msg)
-}
-exports.getKeywords = function() {
-  let keywordFolder = fs.readdirSync(getImageRootPath())
-  let keywords = new Set()
-  keywordFolder.forEach(item => {
-    if (item !== '.DS_Store' && item !== '.gitkeep') {
-      if (item.endsWith(GIF_SUFFIX)) {
-        keywords.add(item.substring(0, item.indexOf(GIF_SUFFIX)))
-      } else {
-        keywords.add(item)
-      }
+    const n = /(_z2C\$q|_z&e3B|AzdH3F)/g
+    const t = /([a-w\d])/g
+    const e = {
+        w: 'a',
+        k: 'b',
+        v: 'c',
+        1: 'd',
+        j: 'e',
+        u: 'f',
+        2: 'g',
+        i: 'h',
+        t: 'i',
+        3: 'j',
+        h: 'k',
+        s: 'l',
+        4: 'm',
+        g: 'n',
+        5: 'o',
+        r: 'p',
+        q: 'q',
+        6: 'r',
+        f: 's',
+        p: 't',
+        7: 'u',
+        e: 'v',
+        o: 'w',
+        8: '1',
+        d: '2',
+        n: '3',
+        9: '4',
+        c: '5',
+        m: '6',
+        0: '7',
+        b: '8',
+        l: '9',
+        a: '0',
+        _z2C$q: ':',
+        '_z&e3B': '.',
+        AzdH3F: '/',
     }
-  })
-  return Array.from(keywords)
+    let o = r.replace(n, function(t, n) {
+        return e[n]
+    })
+    return o.replace(t, function(t, n) {
+        return e[n]
+    })
 }
+function getImagePath() {
+    if (!getSettings('isGif')) {
+        return getImageRootPath() + getSettings('keyword')
+    } else {
+        return getImageRootPath() + getSettings('keyword') + GIF_SUFFIX
+    }
+}
+function log(msg) {
+    if (!out) {
+        out = vscode.window.createOutputChannel('super encourager')
+        out.show()
+    }
+    out.appendLine(msg)
+}
+function getKeywords() {
+    let keywordFolder = fs.readdirSync(getImageRootPath())
+    let keywords = new Set()
+    keywordFolder.forEach(item => {
+        if (item !== '.DS_Store' && item !== '.gitkeep') {
+            if (item.endsWith(GIF_SUFFIX)) {
+                keywords.add(item.substring(0, item.indexOf(GIF_SUFFIX)))
+            } else {
+                keywords.add(item)
+            }
+        }
+    })
+    return Array.from(keywords)
+}
+
+exports.getImageRootPath = getImageRootPath
+exports.getImagePath = getImagePath
+exports.log = log
+exports.getKeywords = getKeywords
