a784d1a86689e4fa41dbee8549d2c1719b901ae7
weiyajun2688 <weiyajun2688@ipalfish.com>
2019-10-21 22:51:20 +0800

    调整template目录

--- src/extension.js
+++ src/extension.js
@@ -23,13 +23,13 @@ function showEncourager(context, imageNames) {
     if (getSettings('isGif')) {
         folderName += GIF_SUFFIX
     }
-    let imagePath = `./images/${folderName}/${name}`
-    const resourcePath = path.join(context.extensionPath, '/index.html')
+    let imagePath = `../../images/${folderName}/${name}`  //这个路径是相对于index.html的位置
+    const resourcePath = path.join(context.extensionPath, '/src/template/index.html')
     const dirPath = path.dirname(resourcePath)
     let html = fs.readFileSync(resourcePath, 'utf-8')
     html = html.replace('$image_path$', imagePath)
     // vscode不支持直接加载本地资源，需要替换成其专有路径格式，这里只是简单的将样式和JS的路径替换
-    html = html.replace(/(<link.+?href='|<img.+?src=')(.+?)'/g, (m, $1, $2) => {
+    html = html.replace(/(<link.+?href='|<img.+?src='|<script.+?src=')(.+?)'/g, (m, $1, $2) => {
         let result =
             $1 +
             vscode.Uri.file(path.resolve(dirPath, $2))
